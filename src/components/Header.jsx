3b2f0af0456bcd2184c0e4fc5f45e70657f152c0
Schrixx <fun_nos@yahoo.com>
2023-11-28 21:56:41 -0700

    feat: complete header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,38 @@
 import Sunny from 'assets/weather/sunny.svg'
+import { v4 as uuidv4 } from 'uuid'
+import { infoGroups } from 'data/infoGroups'
 
 const Header = () => {
     return (
-        <header className="flex justify-center w-full tracking-wide">
-            <div className="rounded-lg max-w-5xl m-8 flex items-center bg-panel">
-                {/* left header */}
-                <div className='flex flex-col items-center bg-panel backdrop-blur-lg rounded-xl'>
-                    <img src={Sunny} className='h-32 w-32 object-contain' data-current-icon />
-                    <div>
-                        <span data-current-temp>31</span>&deg;
-                    </div>
-                </div>
-
-                <span className='mx-5 w-[2px] h-3/4 bg-white' />
-
-                {/* right header */}
-                <div className='relative w-full flex justify-center items-center'>
-                    <div className='rounded-xl'>
-                        <div>
-                            <div>HIGH</div>
-                            <div><span>32</span>&deg;</div>
-                        </div>
-                        <div>
-                            <div>FL HIGH</div>
-                            <div><span>27</span>&deg;</div>
-                        </div>
-                        <div>
-                            <div>WIND</div>
-                            <div><span>9</span><span className="text-base">mph</span></div>
-                        </div>
-                        <div>
-                            <div>LOW</div>
-                            <div><span>19</span>&deg;</div>
-                        </div>
-                        <div>
-                            <div>FL LOW</div>
-                            <div><span>12</span>&deg;</div>
-                        </div>
-                        <div>
-                            <div>PRECIP</div>
-                            <div><span>0.1</span><span className="text-base">in</span></div>
+        <div className='px-4 max-w-5xl m-auto my-16 tracking-wide'>
+            <header className="relative flex justify-center">
+                <div className='absolute top-0 right-0 bottom-0 left-0 -z-10 glassBackground' />
+                <div className="w-full rounded-lg m-8 flex items-center justify-around">
+                    {/* left header */}
+                    <div className='flex items-center'>
+                        <img src={Sunny} className='h-60 w-full object-contain' data-current-icon />
+                        <div className='flex'>
+                            <span className='text-6xl ml-8' data-current-temp>31</span>&deg;
                         </div>
                     </div>
+
+                    <div className='w-[2px] h-3/4 bg-white' />
+
+                    {/* right header */}
+                    <div className='grid grid-cols-3 grid-flow-row gap-4'>
+                        {infoGroups.map((group) => {
+                            return (
+                                <div key={uuidv4()} className='flex flex-col items-center'>
+                                    <div className='text-lg'>{group.label}</div>
+                                    {group.value}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </div>
+
 
     )
 }
---
+++ src/data/infoGroups.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const infoGroups = [
+    {
+        label: "HIGH",
+        value: <div><span>32</span>&deg;</div>,
+    },
+    
+    {
+        label: "FL HIGH",
+        value: <div><span>27</span>&deg;</div>,
+    },
+    
+    {
+        label: "WIND",
+        value: <div><span>9</span><span className="text-base">mph</span></div>,
+    },
+
+    {
+        label: "LOW",
+        value: <div><span>19</span>&deg;</div>,
+    },
+
+    {
+        label: "FL LOW",
+        value: <div><span>12</span>&deg;</div>,
+    },
+
+    {
+        label: "PRECIP",
+        value: <div><span>0.1</span><span className="text-base">in</span></div>,
+    },
+
+]
