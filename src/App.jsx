8df383a01113455a5743c4cf621813f578d512a9
Schrixx <fun_nos@yahoo.com>
2023-11-28 21:57:27 -0700

    refactor: add day cards WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from 'components/Header';
+import DayCards from 'components/DayCards';
 
 function App() {
 
   return (
     <>
       <Header />
+      <DayCards />
     </>
   )
 }
---
+++ src/components/DayCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DayCards = () => {
+  return (
+    <div>DayCards</div>
+  )
+}
+
+export default DayCards
